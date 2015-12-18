4d47e7aa75a7561137b03f223749746fb3eda0bf
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-12-18 01:16:56 +0100

    Code clean-up & fail when all assets are not availables
    Fixes #4

--- index.js
+++ index.js
@@ -5,119 +5,119 @@
  * @author Zach Bruggeman <mail@bruggie.com>
  */
 
-var request = require('request')
-var async = require('async')
-var mime = require('mime')
-var progress = require('progress-stream')
-var util = require('util')
-var fs = require('fs')
-var path = require('path')
 var EventEmitter = require('events').EventEmitter
+var fs           = require('fs')
+var path         = require('path')
+
+var async    = require('async')
+var mime     = require('mime')
+var progress = require('progress-stream')
+var request  = require('request')
+
+const DEFAULT_API_ROOT = 'https://api.github.com'
+
 var pkg = require('./package.json')
 
-var DEFAULT_API_ROOT = 'https://api.github.com'
+
+function noop(){}
+
 
 function PublishRelease (opts, cb) {
-  if (!(this instanceof PublishRelease)) return new PublishRelease(opts, cb)
+  opts = opts || {}
+  cb   = cb   || noop
 
-  this.opts = (opts || {})
-  this.cb = (cb || function noop () {})
-
-  this.publish()
-}
-
-util.inherits(PublishRelease, EventEmitter)
-
-PublishRelease.prototype.publish = function publish () {
-  var self = this
-  var opts = this.opts
-  var cb = this.cb
+  var emitter = new EventEmitter()
 
   // validate opts
   var missing = []
+
   ;['token', 'repo', 'owner', 'tag'].forEach(function validateOpts (opt) {
     if (!opts[opt]) {
       missing.push(opt)
     }
   })
+
   if (missing.length > 0) {
     return cb(new Error('missing required options: ' + missing.join(', ')))
   }
 
-  async.auto({
-    createRelease: function createRelease (callback) {
-      self.emit('create-release')
-      request({
-        uri: util.format((opts.apiUrl || DEFAULT_API_ROOT) + '/repos/%s/%s/releases', opts.owner, opts.repo),
-        method: 'POST',
-        json: true,
-        body: {
-          tag_name: opts.tag,
-          name: opts.name,
-          body: opts.notes,
-          draft: !!opts.draft,
-          prerelease: !!opts.prerelease
-        },
-        headers: {
-          'Authorization': 'token ' + opts.token,
-          'User-Agent': 'publish-release ' + pkg.version + ' (https://github.com/remixz/publish-release)'
-        }
-      }, function (err, res, body) {
-        if (err) return callback(err)
-        self.emit('created-release')
-        callback(null, body)
-      })
+  // Validate assets
+  var assets = opts.assets || []
+
+  async.detect(assets, fs.exists, function(result)
+  {
+    if(result) return callback(result)
+
+    // Create release
+    var headers =
+    {
+      'Authorization': 'token ' + opts.token,
+      'User-Agent': 'publish-release ' + pkg.version + ' (https://github.com/remixz/publish-release)'
+    }
+
+    emitter.emit('create-release')
+
+    request({
+      uri: util.format((opts.apiUrl || DEFAULT_API_ROOT) + '/repos/%s/%s/releases', opts.owner, opts.repo),
+      method: 'POST',
+      json: true,
+      body: {
+        tag_name: opts.tag,
+        name: opts.name,
+        body: opts.notes,
+        draft: !!opts.draft,
+        prerelease: !!opts.prerelease
+      },
+      headers: headers
     },
+    function (err, res, obj) {
+      if (err) return callback(err)
 
-    uploadAssets: ['createRelease', function uploadAssets (callback, obj) {
-      if (!opts.assets || opts.assets.length === 0) return callback()
+      emitter.emit('created-release')
 
-      async.eachSeries(opts.assets, function (asset, callback) {
+      // Upload assets
+      if (!assets.length) return callback()
+
+      async.eachSeries(assets, function (asset, callback) {
         var fileName = path.basename(asset)
-        var uploadUri = obj.createRelease.upload_url.split('{')[0] + '?name=' + fileName
-        self.emit('upload-asset', fileName)
+
+        emitter.emit('upload-asset', fileName)
+
+        var rd = fs.createReadStream(asset)
+        .on('error', callback)
 
         var stat = fs.statSync(asset)
-        var rd = fs.createReadStream(asset)
-        var us = request({
+
+        headers['Content-Length'] = stat.size
+        headers['Content-Type'  ] = mime.lookup(fileName)
+
+        var us = request(
+        {
           method: 'POST',
-          uri: uploadUri,
-          headers: {
-            'Authorization': 'token ' + opts.token,
-            'Content-Type': mime.lookup(fileName),
-            'Content-Length': stat.size,
-            'User-Agent': 'publish-release ' + pkg.version + ' (https://github.com/remixz/publish-release)'
-          }
+          uri: obj.createRelease.upload_url.split('{')[0] + '?name=' + fileName,
+          headers: headers
         })
-
-        var prog = progress({
-            length: stat.size,
-            time: 100
-        }, function (p) {
-          self.emit('upload-progress', fileName, p)
-        })
-
-        rd.on('error', function (err) {
-          callback(err)
-        })
-        us.on('error', function (err) {
-          callback(err)
-        })
-
-        us.on('end', function () {
-          self.emit('uploaded-asset', fileName)
+        .on('error', callback)
+        .on('end', function () {
+          emitter.emit('uploaded-asset', fileName)
           callback()
         })
 
+        var prog = progress(
+        {
+          length: stat.size,
+          time: 100
+        },
+        function (p) {
+          emitter.emit('upload-progress', fileName, p)
+        })
+
         rd.pipe(prog).pipe(us)
-      }, function (err) {
-        callback(err)
-      })
-    }]
-  }, function (err, obj) {
-    if (err) return cb(err)
-    cb(null, obj.createRelease)
+    })
   })
+
+  return emitter
 }
+
 
 module.exports = PublishRelease
